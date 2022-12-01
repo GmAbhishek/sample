148e37fd5686c2fa8409c450d247b9da2b1ba661
Gm_Abhishek <gmabhishek2004@gmail.com>
2022-12-01 10:31:38 +0530

    StateEx and List are added

---
+++ src/component/StateEx.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+export default class StateEx extends Component {
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+         class:1,
+         name:"Gm"
+      }
+    }
+    //inc=()=>{ return this.setState({count:this.State.count+2})}
+  render() {
+    return (
+      <div> 
+      <button onClick={()=>{return this.setState({count:this.state.count+1})}}>Click me{this.state.class}</button>
+      </div>
+    )
+  }
+}
